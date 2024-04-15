d503515577728e412231a2dcdb0242e2ae4094b0
The louie <github@louie.se>
2024-04-15 13:37:00 +0200

    Improve disconnetion handling and do some reorg

--- lewm.ino
+++ lewm.ino
@@ -1,27 +1,40 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+String last_disconnect_reason = "First connection after waking up."; // Communicate the disconnetc reason back so we can put in to mqtt
+char hostnameBuf[28] = {0};
+char bssidBuf[20] = {0};
+
 #include "config.hpp" // Load config
 #include "utils.hpp"  // Some misc functions
 #include "netfunc.hpp" // Pinging and resolving hosts
 #include "wifi.hpp"    // Connecting and reconnecting to wifi
 #include "mqtt.hpp"    // Reconnecting and sending mqtt messages
 
+long wifiInitialConnectTimeout = 60000; // If we don't get a wifi connection withing this time we should reboot
+
 bool firstMsg = true;
-long messageInterval = 300000; // 5 minutes
-long lastMsg = 0;
+long messageInterval = 10000; // 10 seconds
+long messageLastSent = 0;
+
+long timeUpdateLast = 0;
+long timeUpdateInterval = 21600000; // 6 hours
 
 int rtt_gw_ms;
 
 unsigned long reconnectPrevMs = millis();
-unsigned long reconnectInterval = 3600000; // 1 hour
+unsigned long reconnectInterval = 600000; // 10 minutes
 
-String current_timestamp;
+char topicRttBuf[128] = {0};
+char topicRssiBuf[128] = {0};
+
+long timeLastConnected = 0;
 
 void setup()
 {
     Serial.begin(115200);
-    while(!Serial){delay(100);}
+    // while(!Serial){delay(100);}
+    delay(500); // Wait a while for everything to calm down.
 
     Serial.println();
     Serial.println("******************************************************");
@@ -38,7 +51,11 @@ void setup()
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
+        if (millis() > wifiInitialConnectTimeout) {
+          ESP.restart();
+        }
     }
+    timeLastConnected = millis();
     Serial.println("connected");
     bssid = WiFi.BSSIDstr();
     gw_ip = WiFi.gatewayIP();
@@ -51,14 +68,14 @@ void setup()
     Serial.println(bssid);
 
     // Setup NTP to keep the time
-    Serial.print("Setting up NTP");
-    timeClient.begin();
-    timeClient.setTimeOffset(3600);
-    while(!timeClient.isTimeSet()) {
-      Serial.print(".");
-      timeClient.forceUpdate();
-    }
-    Serial.println("success");
+    // Serial.print("Setting up NTP");
+    // timeClient.begin();
+    // timeClient.setTimeOffset(3600);
+    // while(!timeClient.isTimeSet()) {
+    //   Serial.print(".");
+    //   timeClient.forceUpdate();
+    // }
+    // Serial.println("success");
 
     // Connect to MQTT server
     Serial.print("Setting up MQTT...");
@@ -69,92 +86,110 @@ void setup()
 
 void loop(){
   unsigned long now = millis();
-  if (!mqttClient.connected()) {
-    Serial.println("MQTT disconnected");
-    mqttReconnect();
-  }
-  mqttClient.loop();
 
-  // Disconnect from the wifi  periodically to allow for reconnection with alternative base station
-  if ((WiFi.status() == WL_CONNECTED) && (now - reconnectPrevMs > reconnectInterval)) {
-    Serial.print("Disconnecting Wifi...");
-    WiFi.disconnect();
-    Serial.println("Done");
-    sleep(1);
-    reconnectPrevMs = now;
-  }
-
-  // Every now and then do the measurements and send them off
-  if ((WiFi.status() == WL_CONNECTED) && (firstMsg || (now - lastMsg > messageInterval))) {
-    // Use NTP to get the time of day
-    timeClient.update();
-    current_timestamp = getDateTime();
-    Serial.print(current_timestamp); Serial.print("\t");
-
-    // Get the WIFI RSSI
-    int8_t rssi = WiFi.RSSI();
-    Serial.print("RSSI: "); Serial.print(rssi); Serial.print("dB\t");
-
-    // Ping the local gateway
-    Serial.print("GW ("); Serial.print(gw_ip); Serial.print("): ");
-    rtt_gw_ms = pingHost(gw_ip);
-    if (rtt_gw_ms >= 0) {
-      Serial.print(rtt_gw_ms); Serial.print("ms");
-    } else {
-      Serial.print("ERROR");
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println(String(now - timeLastConnected));
+    if (now - timeLastConnected > wifiInitialConnectTimeout) {
+      ESP.restart();
     }
-    Serial.print("\t-\t");
+  } else {
+    timeLastConnected = millis();
+    // if (now - timeUpdateLast > timeUpdateInterval) {
+    //   // Use NTP to get the time of day
+    //   timeClient.update();
+    //   timeUpdateLast = millis();
+    // }
 
-    // Build string
-    char bssidBuf[20] = {0};
-    bssid.toCharArray(bssidBuf, 20);
+    if (!mqttClient.connected()) { 
+      Serial.println("MQTT disconnected");
+      mqttReconnect();
 
-    // Build hostname string
-    char hostnameBuf[28] = {0};
-    hostname.toCharArray(hostnameBuf, 28);
+    } else { 
 
-    // Build a topic strings
-    char topicRttBuf[128] = {0};
-    snprintf(topicRttBuf, 128, "lewm/%s/%s/RTT", hostnameBuf, bssidBuf);
-    char topicRssiBuf[128] = {0};
-    snprintf(topicRssiBuf, 128, "lewm/%s/%s/RSSI", hostnameBuf, bssidBuf);
+      if (last_disconnect_reason != "") {
+        Serial.print("Last disconnect reason: ");
+        Serial.println(last_disconnect_reason);
+        
+        char topic[128] = {0};
+        snprintf(topic, 128, "lewm/%s/status/reconnect_reason", hostnameBuf);
+        Serial.println(topic);
+        char message[255] = {0};
+        snprintf(message, 255, "%s", last_disconnect_reason);
+        mqttClient.publish(topic, message);
+        last_disconnect_reason = "";
+      }
 
-    char mqttMsRttgBuf[8] = {0}; 
-    snprintf(mqttMsRttgBuf, 8, "%d", rtt_gw_ms);
-    char mqttMsgRssiBuf[8] = {0}; 
-    snprintf(mqttMsgRssiBuf, 8, "%d", rssi);
 
-    // Display some debug info
-    Serial.print("Publishing to ");
-    Serial.print(topicRttBuf);
-    Serial.print(": '");
-    Serial.print(mqttMsRttgBuf);
-    Serial.print("'...");
+      // Every now and then do the measurements and send them off
+      if (firstMsg || (now - messageLastSent > messageInterval)) {
+        // Get the WIFI RSSI
+        int8_t rssi = WiFi.RSSI();
+        Serial.print("RSSI: "); Serial.print(rssi); Serial.print("dB\t");
 
-    // Publish the measurements to the MQTT broker
-    bool successRtt = mqttClient.publish(topicRttBuf, mqttMsRttgBuf);
-    if (successRtt) {
-      Serial.println("Success");
-    } else {
-      Serial.println("Fail");
+        // Ping the local gateway
+        Serial.print("GW ("); Serial.print(gw_ip); Serial.print("): ");
+        rtt_gw_ms = pingHost(gw_ip);
+        if (rtt_gw_ms >= 0) {
+          Serial.print(rtt_gw_ms); Serial.print("ms");
+        } else {
+          Serial.print("ERROR");
+        }
+        Serial.print("\t-\t");
+
+        char mqttMsRttgBuf[8] = {0}; 
+        snprintf(mqttMsRttgBuf, 8, "%d", rtt_gw_ms);
+        char mqttMsgRssiBuf[8] = {0}; 
+        snprintf(mqttMsgRssiBuf, 8, "%d", rssi);
+
+        // Build a topic strings
+        snprintf(topicRttBuf, 128, "lewm/%s/%s/RTT", hostnameBuf, bssidBuf);
+        snprintf(topicRssiBuf, 128, "lewm/%s/%s/RSSI", hostnameBuf, bssidBuf);
+        
+        // Display some debug info
+        Serial.print("Publishing to ");
+        Serial.print(topicRttBuf);
+        Serial.print(": '");
+        Serial.print(mqttMsRttgBuf);
+        Serial.print("'...");
+
+        // Publish the measurements to the MQTT broker
+        bool successRtt = mqttClient.publish(topicRttBuf, mqttMsRttgBuf);
+        if (successRtt) {
+          Serial.print("Success");
+        } else {
+          Serial.print("Fail");
+        }
+
+        // Display some debug info
+        Serial.print("\tPublishing to ");
+        Serial.print(topicRssiBuf);
+        Serial.print(": '");
+        Serial.print(mqttMsgRssiBuf);
+        Serial.print("'...");
+
+        // Publish the measurements to the MQTT broker
+        bool successRssi = mqttClient.publish(topicRssiBuf, mqttMsgRssiBuf);
+        if (successRssi) {
+          Serial.print("Success");
+        } else {
+          Serial.print("Fail");
+        }
+
+        Serial.println("");
+
+        firstMsg = false;
+        messageLastSent = now;
+      }
     }
 
-    // Display some debug info
-    Serial.print("Publishing to ");
-    Serial.print(topicRssiBuf);
-    Serial.print(": '");
-    Serial.print(mqttMsgRssiBuf);
-    Serial.print("'...");
-
-    // Publish the measurements to the MQTT broker
-    bool successRssi = mqttClient.publish(topicRssiBuf, mqttMsgRssiBuf);
-    if (successRssi) {
-      Serial.println("Success");
-    } else {
-      Serial.println("Fail");
+    if (now - reconnectPrevMs > reconnectInterval) {
+      // Disconnect from the wifi periodically to allow for reconnection with alternative base station
+      // Rely on automatic reconnection based on the registered events in setup()
+      Serial.print("Disconnecting Wifi...");
+      WiFi.disconnect();
+      Serial.println("Done");
+      sleep(100);
+      reconnectPrevMs = now;
     }
-
-    firstMsg = false;
-    lastMsg = now;
   }
 }
--- wifi.hpp
+++ wifi.hpp
@@ -3,21 +3,26 @@ String hostname;
 IPAddress gw_ip;
 IPAddress local_ip;
 
+
 char node_hostname[255];  // Used in the MQTT code also
 
 void setHostname() {
-    String mac = WiFi.macAddress();
-    hostname = String("lewm-" + mac);
+    hostname = String("lewm-" + WiFi.macAddress());
     hostname.replace(":", "");
     char * cptr = &node_hostname[0];
     FillCstring(cptr, hostname);  // FIXME: Is this needed?
     WiFi.setHostname(node_hostname);
+
+    // Build char array string
+    hostname.toCharArray(hostnameBuf, 28);
+
     Serial.print("Using hostname ");
     Serial.println(hostname);
 }
 
-void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info){
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info) {
   bssid = WiFi.BSSIDstr();
+  bssid.toCharArray(bssidBuf, 20);
   Serial.print("Reconnected to BSSID: ");
   Serial.println(bssid);
 }
@@ -26,6 +31,9 @@ void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
   Serial.println("Disconnected from WiFi access point");
   Serial.print("WiFi lost connection. Reason: ");
   Serial.println(info.wifi_sta_disconnected.reason);
+  last_disconnect_reason = String(info.wifi_sta_disconnected.reason);
+  delay(100);
   Serial.println("Trying to Reconnect");
   WiFi.begin(ssid, password);
+
 }
